75009035b8adb13e2df2814a851db03c8a364661
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-17 21:56:47 +0530

    Add files via upload

--- unit-3/sprint-3/day-1/you/FindCountry.kt
+++ unit-3/sprint-3/day-1/you/FindCountry.kt
@@ -7,6 +7,7 @@ fun main(){
     val day = sc.next();
    val arrayList = arrayListOf<String>("India", "africa" , "new Zealand", "Ghana", "America" ,"Australia")
   if (arrayList.contains(day)){
+      println(arrayList.indexOf(day))
       println("countrty found")
   }else{
       println("not found")
