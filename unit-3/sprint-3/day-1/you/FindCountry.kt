9e15ac4274f8aa1640b7d62ca48ba75704fc3ade
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-17 21:49:47 +0530

    Add files via upload

---
+++ unit-3/sprint-3/day-1/you/FindCountry.kt
@@ -0,0 +1,14 @@
+package YouPart
+
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    val day = sc.next();
+   val arrayList = arrayListOf<String>("India", "africa" , "new Zealand", "Ghana", "America" ,"Australia")
+  if (arrayList.contains(day)){
+      println("countrty found")
+  }else{
+      println("not found")
+  }
+}
