8db70a0ae9267c636ca2eadd48fbc24007739aa4
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-23 15:17:05 +0530

    SplashScreen Added

---
+++ unit-4/evaluation/MusicPlayersprint4/app/src/main/java/com/sumit/musicplayer/SpllashScreen.kt
@@ -0,0 +1,20 @@
+package com.sumit.musicplayer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SpllashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN )
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spllash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext , MainActivity:: class.java))
+        }, 3000)
+
+    }
+}
